c1e2c38dedbc1f44a4c631571634850322af474f
johni08 <johnirevormasiziliwu@gmail.com>
2024-08-16 09:21:00 +0700

    add session provider auth

--- src/providers/redux.tsx
+++ src/providers/redux.tsx
@@ -1,9 +1,14 @@
-"use client"
+"use client";
 import { store } from "@/lib/store";
 import { Provider } from "react-redux";
+import { SessionProvider } from "next-auth/react";
 
-function ReduxProvider({children} : {children: React.ReactNode}) {
-	return <Provider store={store}>{children}</Provider>
+function ReduxProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider>
+      <Provider store={store}>{children}</Provider>
+    </SessionProvider>
+  );
 }
 
 export default ReduxProvider;
