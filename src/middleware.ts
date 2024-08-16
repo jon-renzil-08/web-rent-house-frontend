39419f4a5129c4e809e7974a32ce7ecdb352f478
johni08 <johnirevormasiziliwu@gmail.com>
2024-08-16 13:28:10 +0700

    update next auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/listing/:id/checkout","/booking-success/:path*","/dashboard"] }
+export const config = { matcher: ["/listing/:id/checkout","/booking-success/:path*","/dashboard/:path*"] }
